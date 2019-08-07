353f09140ba9c9689cd7e4cf8202c8c5acd79c42
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-08-07 13:50:49 +0000

    Snapshot of branch master taken on 2019-08-07T09:50:48.336-04:00 from "https://github.com/UnofficialJuliaMirror/Kronecker.jl-2c470bb0-bcc8-11e8-3dad-c9649493f05e"

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,11 @@
-using Kronecker
-using Test
-using LinearAlgebra
+using Kronecker, Test, LinearAlgebra
+using SparseArrays: SparseMatrixCSC
 
-include("testbase.jl")
-include("testindexed.jl")
-include("testeigen.jl")
-include("testkroneckersum.jl")
-include("testfactorization.jl")
-
-#include("testshifted.jl")
+@testset "Kronecker" begin
+    include("testbase.jl")
+    include("testvectrick.jl")
+    include("testindexed.jl")
+    include("testeigen.jl")
+    include("testkroneckersum.jl")
+    include("testfactorization.jl")
+end
